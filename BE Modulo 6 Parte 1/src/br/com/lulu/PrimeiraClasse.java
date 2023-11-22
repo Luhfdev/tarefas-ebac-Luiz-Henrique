e67f7216991d27380c09ee1c46224bb4299da7e8
Luhfdev <luhfdev@gmail.com>
2023-11-22 15:38:00 -0300

    Added exercise for Modulo 6
    Added part 1 and part 2

---
+++ BE Modulo 6 Parte 1/src/br/com/lulu/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+package br.com.lulu;
+
+public class PrimeiraClasse {
+    public static void main(String args[]) {
+        System.out.println("Programação Estruturada:");
+        System.out.println("Onde todo o código fica exclusivamente dentro de um única classe. Fazendo o código funcionar de forma repetitiva.");
+        System.out.println("**********************************");
+        System.out.println("Programação Orientada Objeto:");
+        System.out.println("De forma diferente, essa tipo de programação você tem mais liberdade para trabalhar, podendo organizar e criar quantos metodos precisar para fazer o programa funcionar.");
+    }
+}
--- PrimeiroProjeto/src/br/com/lulu/PrimeiraClasse.java
+++
@@ -1,8 +0,0 @@
-package br.com.lulu;
-
-public class PrimeiraClasse {
-    public static void main(String args[]) {
-        System.out.print("Hello World");
-        System.out.print("Você é fera Gustavo!");
-    }
-}
