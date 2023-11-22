4fd8d00e437a6fe66199aeb80377d042b1ed8400
Luhfdev <139396185+Luhfdev@users.noreply.github.com>
2023-11-22 12:09:48 -0300

    Added exercise mod 4

---
+++ PrimeiroProjeto/src/br/com/lulu/PrimeiraClasse.java
@@ -0,0 +1,8 @@
+package br.com.lulu;
+
+public class PrimeiraClasse {
+    public static void main(String args[]) {
+        System.out.print("Hello World");
+        System.out.print("Você é fera Gustavo!");
+    }
+}
