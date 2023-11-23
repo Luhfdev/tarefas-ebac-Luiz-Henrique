78d1cf0f42a2aede53c9a4f8133e8c58118129a9
Luhfdev <luhfdev@gmail.com>
2023-11-23 13:47:43 -0300

    Added Exercise Method 9

---
+++ Tarefa_Wrapper/ExWrapper/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int n1 = 10;
+        System.out.println(n1);
+        n1 = Integer.valueOf(10);
+        System.out.println(n1);
+    }
+}
