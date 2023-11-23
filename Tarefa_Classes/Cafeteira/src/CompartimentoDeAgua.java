cc36455bbd27f7375751d608ea96dc72690013c0
Luhfdev <luhfdev@gmail.com>
2023-11-22 22:59:55 -0300

    Adicionado compartimento de agua

---
+++ Tarefa_Classes/Cafeteira/src/CompartimentoDeAgua.java
@@ -0,0 +1,13 @@
+public class CompartimentoDeAgua {
+
+    private int capacidadeA;
+
+    public int getCapacidadeA() {
+        return capacidadeA;
+    }
+
+    public void setCapacidadeA(int capacidadeA) {
+        this.capacidadeA = capacidadeA;
+    }
+}
+
