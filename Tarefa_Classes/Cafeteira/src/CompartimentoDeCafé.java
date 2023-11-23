aa335d360a2399ac37f354c8bf5b089892b25cdb
Luhfdev <luhfdev@gmail.com>
2023-11-22 23:00:03 -0300

    Adicionado compartimento de café

---
+++ Tarefa_Classes/Cafeteira/src/CompartimentoDeCafé.java
@@ -0,0 +1,12 @@
+public class CompartimentoDeCafé {
+
+    private int capacidadeC;
+
+    public int getCapacidadeC() {
+        return capacidadeC;
+    }
+
+    public void setCapacidadeC(int capacidadeC) {
+        this.capacidadeC = capacidadeC;
+    }
+}
