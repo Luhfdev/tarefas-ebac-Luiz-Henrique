b6136f73ba92932d7baf3606ca0ce37340608be3
Luhfdev <luhfdev@gmail.com>
2024-04-29 20:11:54 -0300

    Ok

---
+++ tarefa_mod30/src/br/com/luhf/dao/IProdutoDAO.java
@@ -0,0 +1,8 @@
+package br.com.luhf.dao;
+
+import br.com.luhf.dao.generic.IGenericDAO;
+import br.com.luhf.domain.Produto;
+
+public interface IProdutoDAO extends IGenericDAO<Produto, String>{
+
+}
